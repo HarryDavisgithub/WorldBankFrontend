1d9b078e8200340a7d72092ae97b0ec1b45ffb05
Harry Davis <61976914+HarryDavisgithub@users.noreply.github.com>
2022-02-15 17:21:31 +0000

    Connected search to networking

--- world-bank-app/src/components/Search.js
+++ world-bank-app/src/components/Search.js
@@ -1,19 +1,23 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Form, Button } from "react-bootstrap";
 import { YearRangePicker } from "react-year-range-picker";
+import Network from "../Network.js";
 
 export default function Search() {
-	const [countries, setCountries] = useState([
-		"United Kingdom",
-		"France",
-		"Germany",
-		"Spain",
-	]); //store as props from APP in future
-	const [indicators, setIndicators] = useState([1, 2, 3, 4]);
+	const network = new Network();
+	const [countries, setCountries] = useState();
+	const [indicators, setIndicators] = useState();
 	const [selectedCountry, setSelectedCountry] = useState();
 	const [selectedIndicator, setSelectedIndicator] = useState();
 
 	const [yearRange, setYearRange] = useState();
+
+	useEffect(() => {
+		(async () => {
+			setCountries(await network.getCountries());
+			setIndicators(await network.getIndicators());
+		})();
+	}, []);
 
 	function createCountryList() {
 		return countries.map((country, key) => (
@@ -33,65 +37,74 @@ export default function Search() {
 
 	function printInfo(e) {
 		e.preventDefault();
+		console.log(countries);
 		console.log(selectedCountry);
 		console.log(selectedIndicator);
 		console.log(yearRange);
 	}
 
 	return (
-		<div className="selection-bar">
-			<div>
-				<Form.Label>Select a country</Form.Label>
-				<Form.Select
-					className="bar-item"
-					aria-label="Country-list"
-					onChange={(e) => setSelectedCountry(e.target.value)}
-					style={{ width: 200 }}
-				>
-					{createCountryList()}
-				</Form.Select>
-			</div>
-			<div>
-				<Form.Label>Select a indicator</Form.Label>
-				<Form.Select
-					className="bar-item"
-					aria-label="Indicator-list"
-					onChange={(e) => setSelectedIndicator(e.target.value)}
-					style={{ width: 200 }}
-				>
-					{createIndicatorList()}
-				</Form.Select>
-			</div>
+		<div>
+			{countries !== undefined && indicators !== undefined ? (
+				<div className="selection-bar">
+					<div>
+						<Form.Label>Select a country</Form.Label>
+						<Form.Select
+							className="bar-item"
+							aria-label="Country-list"
+							onChange={(e) =>
+								setSelectedCountry(e.target.value)
+							}
+							style={{ width: 200 }}
+						>
+							{createCountryList()}
+						</Form.Select>
+					</div>
+					<div>
+						<Form.Label>Select a indicator</Form.Label>
+						<Form.Select
+							className="bar-item"
+							aria-label="Indicator-list"
+							onChange={(e) =>
+								setSelectedIndicator(e.target.value)
+							}
+							style={{ width: 200 }}
+						>
+							{createIndicatorList()}
+						</Form.Select>
+					</div>
 
-			<div className="year-range-container">
-				<Form.Label style={{ color: "white" }}>
-					Year Range
-				</Form.Label>
-				<YearRangePicker
-					id="year-range"
-					minYear="1960"
-					maxYear="2015"
-					onSelect={(startYear, endYear) => {
-						setYearRange({
-							startYear,
-							endYear,
-						});
-					}}
-					startYear={yearRange?.startYear}
-					endYear={yearRange?.endYear}
-					classNames="bar-item custom-year-range-picker"
-					selectedColor="#0963b5"
-				/>
-			</div>
+					<div className="year-range-container">
+						<Form.Label style={{ color: "white" }}>
+							Year Range
+						</Form.Label>
+						<YearRangePicker
+							id="year-range"
+							minYear="1960"
+							maxYear="2015"
+							onSelect={(startYear, endYear) => {
+								setYearRange({
+									startYear,
+									endYear,
+								});
+							}}
+							startYear={yearRange?.startYear}
+							endYear={yearRange?.endYear}
+							classNames="bar-item custom-year-range-picker"
+							selectedColor="#0963b5"
+						/>
+					</div>
 
-			<Button
-				variant="primary"
-				type="submit"
-				onClick={printInfo}
-				className="bar-item search-button"
-			>
-				Submit
-			</Button>
+					<Button
+						variant="primary"
+						type="submit"
+						onClick={printInfo}
+						className="bar-item search-button"
+					>
+						Submit
+					</Button>
+				</div>
+			) : null}
 		</div>
 	);
 }
