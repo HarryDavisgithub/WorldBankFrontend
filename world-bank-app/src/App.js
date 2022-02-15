844fd6d58211bfa671a6da3e5ba1dadd6330529d
Harry Davis <61976914+HarryDavisgithub@users.noreply.github.com>
2022-02-15 12:13:21 +0000

    Improved alignment of input boxes

--- world-bank-app/src/App.js
+++ world-bank-app/src/App.js
@@ -1,8 +1,6 @@
 import "./App.css";
 import Search from "./Search";
-import Button from "react-bootstrap/Button";
 import "bootstrap/dist/css/bootstrap.min.css";
-import Test2 from "./Test2";
 
 function App() {
 	return (
--- world-bank-app/src/Search.js
+++ world-bank-app/src/Search.js
@@ -1,26 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Form, Button, Col, Row } from "react-bootstrap";
 import { YearRangePicker } from "react-year-range-picker";
 
 export default function Search() {
-	const [country, setCountry] = useState();
-	const [indicator, setIndicator] = useState();
+	const [countries, setCountries] = useState([
+		"United Kingdom",
+		"France",
+		"Germany",
+		"Spain",
+	]);
+	const [indicators, setIndicators] = useState([1, 2, 3, 4]);
+	const [selectedCountry, setSelectedCountry] = useState();
+	const [selectedIndicator, setSelectedIndicator] = useState();
+
 	const [yearRange, setYearRange] = useState();
 
+	// useEffect(() => {
+	// 	const countryList = ["United Kingdom", "France", "Germany", "Spain"]; //get from networking
+	// 	const indicatorList = [1, 2, 3, 4];
+	// 	setCountries(countryList);
+	// 	setIndicators(indicatorList);
+	// 	console.log("hello");
+	// }, []);
+
 	function createCountryList() {
-		const countries = ["United Kingdom", "France", "Germany", "Spain"];
-		//const countries = getCounties() //make networking class
 		return countries.map((country, key) => (
 			<option key={key} value={country}>
 				{country}
 			</option>
-			// <Radio key={key} type="checkbox" label={country} />
 		));
 	}
 
 	function createIndicatorList() {
-		const indicators = [1, 2, 3, 4];
-		//const indicators = getIndicators() //make networking class
 		return indicators.map((indicator, key) => (
 			<option key={key} value={indicator}>
 				{indicator}
@@ -28,53 +39,79 @@ export default function Search() {
 		));
 	}
 
+	function printInfo(e) {
+		e.preventDefault();
+		console.log(selectedCountry);
+		console.log(selectedIndicator);
+		console.log(yearRange);
+	}
+
 	return (
-		<>
+		<div className="selection-bar">
 			<Form>
 				<Row className="mb-3">
-					<Col>
-						<Form.Label>Select a country</Form.Label>
+					<Col xs={3}>
+						{/* <Form.Label>Select a country</Form.Label> */}
 						<Form.Select
 							aria-label="Country-list"
-							onChange={(e) => setCountry(e.target.value)}
+							onChange={(e) =>
+								setSelectedCountry(e.target.value)
+							}
 						>
 							{createCountryList()}
 						</Form.Select>
 					</Col>
-					<Col>
-						<Form.Label>Select a indicator</Form.Label>
+					<Col xs={3}>
+						{/* <Form.Label>Select a indicator</Form.Label> */}
 						<Form.Select
 							aria-label="Indicator-list"
 							onChange={(e) =>
-								setIndicator(e.target.value)
+								setSelectedIndicator(e.target.value)
 							}
 						>
 							{createIndicatorList()}
 						</Form.Select>
 					</Col>
 
-					<Button variant="primary" type="submit">
-						Submit
-					</Button>
+					<Col>
+						<div
+							style={{
+								color: "black",
+							}}
+						>
+							<YearRangePicker
+								minYear="1960"
+								maxYear={new Date().getFullYear()}
+								onSelect={(startYear, endYear) => {
+									setYearRange({
+										startYear,
+										endYear,
+									});
+								}}
+								startYear={yearRange?.startYear}
+								endYear={yearRange?.endYear}
+								style={{
+									maxWidth: "200px",
+									width: "100%",
+
+									border: 0,
+								}}
+								classNames="custom-year-range-picker"
+								selectedColor="#0963b5"
+							/>
+						</div>
+					</Col>
+					<Col>
+						<Button
+							variant="primary"
+							type="submit"
+							onClick={printInfo}
+						>
+							Submit
+						</Button>
+					</Col>
 				</Row>
 			</Form>
-
-			<div>
-				<h2>Basic Usage</h2>
-				<YearRangePicker
-					minYear="1960"
-					maxYear={new Date().getFullYear()}
-					onSelect={(startYear, endYear) => {
-						setYearRange({ startYear, endYear });
-					}}
-					startYear={yearRange?.startYear}
-					endYear={yearRange?.endYear}
-				/>
-				<span style={{ marginLeft: "16px" }}>
-					Selected Years : {yearRange?.startYear} -{" "}
-					{yearRange?.endYear}
-				</span>
-			</div>
-		</>
+		</div>
 	);
 }
