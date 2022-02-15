5c8a0a4ba3c230a59e772ed67f17d5f308085d26
Oliver Case-Green <olivercase-green@hotmail.co.uk>
2022-02-15 14:19:29 +0000

    Added network file

---
+++ world-bank-app/src/Network.js
@@ -0,0 +1,34 @@
+import React from "react";
+
+export default class Networking extends React.Component {
+  async getCountries() {
+    const response = await fetch("http://localhost:8080/countries");
+    return await response.json();
+  }
+
+  async getIndicators() {
+    const response = await fetch("http://localhost:8080/indicators");
+    return await response.json();
+  }
+
+  async getCountryInfo(country) {
+    const response = await fetch(
+      `http://localhost:8080/countries/${country}/info`
+    );
+    return await response.json();
+  }
+
+  async getCountryIndicatorInfo(country, indicator) {
+    const response = await fetch(
+      `http://localhost:8080/countries/${country}/${indicator}/info`
+    );
+    return await response.json();
+  }
+
+  async getCountryIndicatorInfo(country, indicator, year) {
+    const response = await fetch(
+      `http://localhost:8080/countries/${country}/${indicator}/${year}/info`
+    );
+    return await response.json();
+  }
+}
