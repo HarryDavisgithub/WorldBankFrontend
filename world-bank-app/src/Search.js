a93e5abee9538275c1ec2a26bfcf0b007528bde4
Harry Davis <61976914+HarryDavisgithub@users.noreply.github.com>
2022-02-15 14:03:25 +0000

    Removed unused code

--- world-bank-app/src/Search.js
+++ world-bank-app/src/Search.js
@@ -8,20 +8,12 @@ export default function Search() {
 		"France",
 		"Germany",
 		"Spain",
-	]);
+	]); //store as props from APP in future
 	const [indicators, setIndicators] = useState([1, 2, 3, 4]);
 	const [selectedCountry, setSelectedCountry] = useState();
 	const [selectedIndicator, setSelectedIndicator] = useState();
 
 	const [yearRange, setYearRange] = useState();
-
-	// useEffect(() => {
-	// 	const countryList = ["United Kingdom", "France", "Germany", "Spain"]; //get from networking
-	// 	const indicatorList = [1, 2, 3, 4];
-	// 	setCountries(countryList);
-	// 	setIndicators(indicatorList);
-	// 	console.log("hello");
-	// }, []);
 
 	function createCountryList() {
 		return countries.map((country, key) => (
@@ -48,70 +40,63 @@ export default function Search() {
 
 	return (
 		<div className="selection-bar">
-			<Form>
-				<Row className="mb-3">
-					<Col xs={3}>
-						{/* <Form.Label>Select a country</Form.Label> */}
-						<Form.Select
-							aria-label="Country-list"
-							onChange={(e) =>
-								setSelectedCountry(e.target.value)
-							}
-						>
-							{createCountryList()}
-						</Form.Select>
-					</Col>
-					<Col xs={3}>
-						{/* <Form.Label>Select a indicator</Form.Label> */}
-						<Form.Select
-							aria-label="Indicator-list"
-							onChange={(e) =>
-								setSelectedIndicator(e.target.value)
-							}
-						>
-							{createIndicatorList()}
-						</Form.Select>
-					</Col>
+			<div>
+				{/* <Form.Label>Select a country</Form.Label> */}
+				<Form.Select
+					className="bar-item"
+					aria-label="Country-list"
+					onChange={(e) => setSelectedCountry(e.target.value)}
+					style={{ width: 200 }}
+				>
+					{createCountryList()}
+				</Form.Select>
+			</div>
+			<div>
+				{/* <Form.Label>Select a indicator</Form.Label> */}
+				<Form.Select
+					className="bar-item"
+					aria-label="Indicator-list"
+					onChange={(e) => setSelectedIndicator(e.target.value)}
+					style={{ width: 200 }}
+				>
+					{createIndicatorList()}
+				</Form.Select>
+			</div>
 
-					<Col>
-						<div
-							style={{
-								color: "black",
-							}}
-						>
-							<YearRangePicker
-								minYear="1960"
-								maxYear={new Date().getFullYear()}
-								onSelect={(startYear, endYear) => {
-									setYearRange({
-										startYear,
-										endYear,
-									});
-								}}
-								startYear={yearRange?.startYear}
-								endYear={yearRange?.endYear}
-								style={{
-									maxWidth: "200px",
-									width: "100%",
+			<div
+				style={{
+					color: "black",
+				}}
+			>
+				<YearRangePicker
+					minYear="1960"
+					maxYear={new Date().getFullYear()}
+					onSelect={(startYear, endYear) => {
+						setYearRange({
+							startYear,
+							endYear,
+						});
+					}}
+					startYear={yearRange?.startYear}
+					endYear={yearRange?.endYear}
+					style={{
+						// maxWidth: "200px",
+						// width: "100%",
+						border: 0,
+					}}
+					classNames="bar-item custom-year-range-picker"
+					selectedColor="#0963b5"
+				/>
+			</div>
 
-									border: 0,
-								}}
-								classNames="custom-year-range-picker"
-								selectedColor="#0963b5"
-							/>
-						</div>
-					</Col>
-					<Col>
-						<Button
-							variant="primary"
-							type="submit"
-							onClick={printInfo}
-						>
-							Submit
-						</Button>
-					</Col>
-				</Row>
-			</Form>
+			<Button
+				variant="primary"
+				type="submit"
+				onClick={printInfo}
+				className="bar-item"
+			>
+				Submit
+			</Button>
 		</div>
 	);
 }
