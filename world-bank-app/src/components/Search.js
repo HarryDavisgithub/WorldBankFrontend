d0c93b34c5920972553b632d0e2e1eb6f772159c
Harry Davis <61976914+HarryDavisgithub@users.noreply.github.com>
2022-02-16 14:43:50 +0000

    Added multi country selection

--- world-bank-app/src/components/Search.js
+++ world-bank-app/src/components/Search.js
@@ -3,14 +3,19 @@ import { Form, Button } from "react-bootstrap";
 import { YearRangePicker } from "react-year-range-picker";
 import Network from "../Network.js";
 
-export default function Search() {
+import "antd/dist/antd.css";
+
+import { Select } from "antd";
+
+export default function Search(props) {
 	const network = new Network();
 	const [countries, setCountries] = useState();
 	const [indicators, setIndicators] = useState();
-	const [selectedCountry, setSelectedCountry] = useState();
-	const [selectedIndicator, setSelectedIndicator] = useState();
-
+	const [selectedCountries, setSelectedCountries] = useState([]);
+	const [selectedIndicator, setSelectedIndicator] = useState("All");
 	const [yearRange, setYearRange] = useState();
+
+	const { Option } = Select;
 
 	useEffect(() => {
 		(async () => {
@@ -18,14 +23,6 @@ export default function Search() {
 			setIndicators(await network.getIndicators());
 		})();
 	}, []);
-
-	function createCountryList() {
-		return countries.map((country, key) => (
-			<option key={key} value={country}>
-				{country}
-			</option>
-		));
-	}
 
 	function createIndicatorList() {
 		return indicators.map((indicator, key) => (
@@ -35,12 +32,13 @@ export default function Search() {
 		));
 	}
 
-	function printInfo(e) {
+	function returnSelection(e) {
 		e.preventDefault();
-		console.log(countries);
-		console.log(selectedCountry);
 		console.log(selectedIndicator);
-		console.log(yearRange);
+	}
+
+	function handleCountriesChange(country) {
+		setSelectedCountries(country);
 	}
 
 	return (
@@ -49,35 +47,44 @@ export default function Search() {
 				<div className="selection-bar">
 					<div>
 						<Form.Label>Select a country</Form.Label>
-						<Form.Select
+						<Select
 							className="bar-item"
-							aria-label="Country-list"
-							onChange={(e) =>
-								setSelectedCountry(e.target.value)
-							}
-							style={{ width: 200 }}
+							mode="multiple"
+							style={{ width: "100%" }}
+							placeholder="Please select up to two countries"
+							onChange={handleCountriesChange}
 						>
-							{createCountryList()}
-						</Form.Select>
+							{countries.map((item) => (
+								<Option
+									disabled={
+										selectedCountries.length > 1
+											? selectedCountries.includes(item)
+												? false
+												: true
+											: false
+									}
+									key={item}
+								>
+									{item}
+								</Option>
+							))}
+						</Select>
 					</div>
 					<div>
 						<Form.Label>Select a indicator</Form.Label>
 						<Form.Select
 							className="bar-item"
 							aria-label="Indicator-list"
-							onChange={(e) =>
-								setSelectedIndicator(e.target.value)
-							}
+							onChange={(e) => setSelectedIndicator(e.target.value)}
 							style={{ width: 200 }}
 						>
+							<option value="All">All</option>
 							{createIndicatorList()}
 						</Form.Select>
 					</div>
 
 					<div className="year-range-container">
-						<Form.Label style={{ color: "white" }}>
-							Year Range
-						</Form.Label>
+						<Form.Label style={{ color: "white" }}>Year Range</Form.Label>
 						<YearRangePicker
 							id="year-range"
 							minYear="1960"
@@ -98,7 +105,7 @@ export default function Search() {
 					<Button
 						variant="primary"
 						type="submit"
-						onClick={printInfo}
+						onClick={returnSelection}
 						className="bar-item search-button"
 					>
 						Submit
