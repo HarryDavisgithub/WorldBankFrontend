3f0e105f511b422c757a5be50e1d52f27af46d5e
Harry Davis <61976914+HarryDavisgithub@users.noreply.github.com>
2022-02-17 12:13:40 +0000

    Fixed issues with app

--- world-bank-app/src/App.js
+++ world-bank-app/src/App.js
@@ -5,78 +5,72 @@ import MainPage from "./components/MainPage";
 import { useCookies } from "react-cookie";
 import React, { useState, useEffect } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
-
-function App() {
-  const [cookies, setCookie] = useCookies(["sessionId"]);
-  const [isLoggedIn, setIsLoggedIn] = useState(cookies.sessionId);
-
-  useEffect(() => {
-    console.log(isLoggedIn);
-  }, [cookies.sessionId, isLoggedIn]);
-
-  const deleteCookiesOnLogOut = () => {
-    setCookie("sessionId", "");
-    setIsLoggedIn("");
-  };
-
-  return (
-    <Routes>
-      <Route
-        path="/"
-        element={
-          !isLoggedIn ? (
-            <MainPage setIsLoggedIn={setIsLoggedIn} />
-          ) : (
-            <Navigate replace to="/search" />
-          )
-        }
-      />
-      <Route
-        path="/search"
-        element={
-          isLoggedIn ? (
-            <button onClick={deleteCookiesOnLogOut}>hello</button>
-          ) : (
-            <Navigate replace to="/" />
-          )
-        }
-      />
-    </Routes>
-import { useState } from "react";
 import OneCountryOneIndicator from "./components/OneCountryOneIndicator";
 
 function App() {
-  const [selectedCountries, setSelectedCountries] = useState();
-  const [selectedIndicator, setSelectedIndicator] = useState();
-  const [selectedYear, setSelectedYear] = useState();
+	const [cookies, setCookie] = useCookies(["sessionId"]);
+	const [isLoggedIn, setIsLoggedIn] = useState(cookies.sessionId);
 
-  function getSelectedCountries(countries) {
-    setSelectedCountries(countries);
-    console.log(countries);
-  }
+	const [selectedCountries, setSelectedCountries] = useState();
+	const [selectedIndicator, setSelectedIndicator] = useState();
+	const [selectedYear, setSelectedYear] = useState();
 
-  function getSelectedIndicators(indicator) {
-    setSelectedIndicator(indicator);
-    console.log(indicator);
-  }
+	useEffect(() => {
+		console.log(isLoggedIn);
+	}, [cookies.sessionId, isLoggedIn]);
 
-  function getSelectedYear(year) {
-    setSelectedYear(year);
-    console.log(year);
-  }
+	const deleteCookiesOnLogOut = () => {
+		setCookie("sessionId", "");
+		setIsLoggedIn("");
+	};
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        {/* <Search
-          getSelectedCountries={getSelectedCountries}
-          getSelectedIndicators={getSelectedIndicators}
-          getSelectedYear={getSelectedYear}
-        /> */}
-        <OneCountryOneIndicator country={"Albania"} indicator={"Indicator"} />
-      </header>
-    </div>
-  );
+	function getSelectedCountries(countries) {
+		setSelectedCountries(countries);
+		console.log(countries);
+	}
+
+	function getSelectedIndicators(indicator) {
+		setSelectedIndicator(indicator);
+		console.log(indicator);
+	}
+
+	function getSelectedYear(year) {
+		setSelectedYear(year);
+		console.log(year);
+	}
+
+	return (
+		<Routes>
+			<Route
+				path="/"
+				element={
+					!isLoggedIn ? (
+						<MainPage setIsLoggedIn={setIsLoggedIn} />
+					) : (
+						<>
+							<Navigate replace to="/search" />
+						</>
+					)
+				}
+			/>
+			<Route
+				path="/search"
+				element={
+					isLoggedIn ? (
+						<Search
+							getSelectedCountries={getSelectedCountries}
+							getSelectedIndicators={getSelectedIndicators}
+							getSelectedYear={getSelectedYear}
+						/>
+					) : (
+						<Navigate replace to="/" />
+					)
+				}
+			/>
+		</Routes>
+
+		//<OneCountryOneIndicator country={"Albania"} indicator={"Indicator"} />
+	);
 }
 
 export default App;
