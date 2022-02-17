bd47a138f9c6232cca025840f3a939918523564f
Harry Davis <61976914+HarryDavisgithub@users.noreply.github.com>
2022-02-17 11:19:37 +0000

    Recreated selects in AntD to improve functionality

--- world-bank-app/src/components/Search.js
+++ world-bank-app/src/components/Search.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
-import { Form, Button } from "react-bootstrap";
+import { Form, Button, Container, Navbar } from "react-bootstrap";
 import Network from "../Network.js";
+import Logo from "../assets/bank-logo.png";
 
 import "antd/dist/antd.css";
 import { Select } from "antd";
@@ -55,94 +56,130 @@ export default function Search(props) {
 		setSelectedCountries(country);
 	}
 
-	return (
-		<div>
-			{countries !== undefined && indicators !== undefined ? (
-				<div className="selection-bar">
-					<div>
-						<Form.Label>Select a country</Form.Label>
-						<Select
-							className="bar-item"
-							dropdownStyle={{ minWidth: "20%" }}
-							mode="multiple"
-							placeholder="Please select up to two countries"
-							onChange={handleCountriesChange}
-						>
-							{countries.map((item) => (
-								<Option
-									disabled={
-										selectedCountries.length > 1
-											? selectedCountries.includes(item)
-												? false
-												: true
-											: false
-									}
-									key={item}
-								>
-									{item}
-								</Option>
-							))}
-						</Select>
-					</div>
-					<div>
-						<Form.Label>Select a indicator</Form.Label>
-						<Select
-							showSearch
-							dropdownStyle={{ minWidth: "50%" }}
-							// style={{ minWidth: "50%" }}
-							placeholder="Select a indicator"
-							optionFilterProp="children"
-							onChange={(e) => setSelectedIndicator(e.target.value)}
-							filterOption={(input, option) =>
-								option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+	function getCountrySelect(e) {
+		return (
+			<div>
+				<Form.Label>Select a country</Form.Label>
+				<Select
+					dropdownStyle={{ minWidth: "20%" }}
+					style={{ minWidth: "100%" }}
+					mode="multiple"
+					placeholder="Please select up to two countries"
+					onChange={handleCountriesChange}
+				>
+					{countries.map((item) => (
+						<Option
+							disabled={
+								selectedCountries.length > 1
+									? selectedCountries.includes(item)
+										? false
+										: true
+									: false
 							}
+							key={item}
 						>
-							{createIndicatorList()}
-						</Select>
-						{/* <Form.Select
-							className="bar-item"
-							aria-label="Indicator-list"
-							onChange={(e) => setSelectedIndicator(e.target.value)}
-							style={{ width: 200 }}
-						>
-							<option value="All">All</option>
-							{createIndicatorList()}
-						</Form.Select> */}
-					</div>
-					<div>
-						<Form.Label>Select a year</Form.Label>
-						{/* <Form.Select
-							className="bar-item"
-							aria-label="Year-list"
-							onChange={(e) => setSelectedYear(e.target.value)}
-							style={{ width: 200 }}
-						>
-							<option value="All">All</option>
-							{createYearsList()}
-						</Form.Select> */}
-						<Select
-							showSearch
-							placeholder="Select a year"
-							optionFilterProp="children"
-							onChange={(e) => setSelectedYear(e.target.value)}
-							filterOption={(input, option) =>
-								option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
-							}
-						>
-							{createYearsList()}
-						</Select>
-					</div>
+							{item}
+						</Option>
+					))}
+				</Select>
+			</div>
+		);
+	}
 
-					<Button
-						variant="primary"
-						type="submit"
-						onClick={returnSelection}
-						className="bar-item search-button"
-					>
-						Submit
-					</Button>
-				</div>
-			) : null}
-		</div>
+	function handleIndicatorChange(indicator) {
+		setSelectedIndicator(indicator);
+	}
+
+	function getIndicatorSelect(e) {
+		return (
+			<div>
+				<Form.Label>Select a indicator</Form.Label>
+				<Select
+					className="search-select"
+					showSearch
+					dropdownStyle={{ minWidth: "50%" }}
+					style={{ minWidth: "100%" }}
+					placeholder="Select a indicator"
+					optionFilterProp="children"
+					onChange={handleIndicatorChange}
+					filterOption={(input, option) =>
+						option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+					}
+				>
+					<Select.Option value="All">All</Select.Option>
+					{createIndicatorList()}
+				</Select>
+			</div>
+		);
+	}
+
+	function handleYearChange(year) {
+		setSelectedYear(year);
+	}
+
+	function getYearSelect(e) {
+		return (
+			<div>
+				<Form.Label>Select a year</Form.Label>
+
+				<Select
+					showSearch
+					placeholder="Select a year"
+					style={{ minWidth: "100%" }}
+					optionFilterProp="children"
+					onChange={handleYearChange}
+					filterOption={(input, option) =>
+						option.children.toLowerCase().indexOf(input.toLowerCase()) >= 0
+					}
+				>
+					{" "}
+					<Select.Option value="All">All</Select.Option>
+					{createYearsList()}
+				</Select>
+			</div>
+		);
+	}
+
+	return (
+		<Container className="py-4">
+			<header className="border-bottom pb-3 mb-4">
+				<Navbar>
+					<Container>
+						<Navbar.Brand href="#home">
+							<img src={Logo} height={50} alt="world-bank-logo" />
+						</Navbar.Brand>
+					</Container>
+				</Navbar>
+			</header>
+			<div className="p-5 mb-4 bg-light rounded-3">
+				<Container className="py-5">
+					{countries && indicators ? (
+						<div className="selection-bar">
+							{getCountrySelect()}
+							{getIndicatorSelect()}
+							{getYearSelect()}
+							<Button
+								variant="primary"
+								type="submit"
+								onClick={returnSelection}
+								className="bar-item search-button"
+							>
+								Submit
+							</Button>
+						</div>
+					) : (
+						<p>Loading...</p>
+					)}
+				</Container>
+			</div>
+			<Container>
+				<footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+					<p className="col-md-4 mb-0 text-muted">© 2022 Team Delta</p>
+					<div className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+						<img className="bi me-2" height="32" src={Logo} alt="world-bank-logo"></img>
+					</div>
+				</footer>
+			</Container>
+		</Container>
 	);
 }
