9b74e8550dd19d9fc28f53cffda67e2ca673b1da
TamoorW <waheedtamoor@gmail.com>
2022-02-18 12:04:07 +0000

    added log out every page and disabled submit button

--- world-bank-app/src/components/OneCountryOneIndicator.js
+++ world-bank-app/src/components/OneCountryOneIndicator.js
@@ -1,5 +1,13 @@
 import { useState, useEffect } from "react";
-import { LineChart, Line, XAxis, YAxis, CartesianGrid, Tooltip, Label } from "recharts";
+import {
+  LineChart,
+  Line,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Label,
+} from "recharts";
 import Navbar from "react-bootstrap/Navbar";
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
@@ -7,73 +15,91 @@ import Network from "../Network.js";
 import Logo from "../assets/bank-logo.png";
 
 export default function OneCountryOneIndicator(props) {
-	const network = new Network();
-	const [data, setData] = useState();
+  const network = new Network();
+  const [data, setData] = useState();
 
-	useEffect(() => {
-		(async () => {
-			setData(await network.getCountryIndicatorInfo(props.country[0], props.indicator));
-		})();
-	}, []);
+  useEffect(() => {
+    (async () => {
+      setData(
+        await network.getCountryIndicatorInfo(props.country[0], props.indicator)
+      );
+    })();
+  }, []);
 
-	function getGraph() {
-		if ("response" in data) {
-			return <p>Please choose a different country and indicator combination</p>;
-		} else {
-			return (
-				<Container className="py-5">
-					{console.log(data)}
-					<h2>{props.country}</h2>
-					<h4>{props.indicator}</h4>
-					<LineChart
-						width={1150}
-						height={400}
-						data={data}
-						margin={{
-							top: 5,
-							right: 0,
-							left: 0,
-							bottom: 25,
-						}}
-					>
-						<CartesianGrid strokeDasharray="3 3" />
-						<XAxis dataKey="Year">
-							<Label value="Year" position="insideBottom" offset={-20} />
-						</XAxis>{" "}
-						<YAxis />
-						<Tooltip />
-						<Line type="monotone" dataKey="Value" stroke="#8884d8" activeDot={{ r: 8 }} />
-					</LineChart>
-				</Container>
-			);
-		}
-	}
+  function getGraph() {
+    if ("response" in data) {
+      return <p>Please choose a different country and indicator combination</p>;
+    } else {
+      return (
+        <Container className="py-5">
+          {console.log(data)}
+          <h2>{props.country}</h2>
+          <h4>{props.indicator}</h4>
+          <LineChart
+            width={1150}
+            height={400}
+            data={data}
+            margin={{
+              top: 5,
+              right: 0,
+              left: 0,
+              bottom: 25,
+            }}
+          >
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="Year">
+              <Label value="Year" position="insideBottom" offset={-20} />
+            </XAxis>{" "}
+            <YAxis />
+            <Tooltip />
+            <Line
+              type="monotone"
+              dataKey="Value"
+              stroke="#8884d8"
+              activeDot={{ r: 8 }}
+            />
+          </LineChart>
+        </Container>
+      );
+    }
+  }
 
-	return (
-		<Container className="py-4">
-			<header className="border-bottom pb-3 mb-4">
-				<Navbar>
-					<Container>
-						<Navbar.Brand href="/">
-							<img src={Logo} height={50} alt="world-bank-logo" />{" "}
-							<Button variant="primary">Search</Button>{" "}
-							<Button variant="primary">History</Button>
-						</Navbar.Brand>
-					</Container>
-					<Button variant="primary" className="text-nowrap">
-						Log Out
-					</Button>
-				</Navbar>
-			</header>
-			<div className="mb-4 bg-light rounded-3">{data ? getGraph() : <p>Loading...</p>}</div>
-			<Container>
-				<footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-					<p className="col-md-4 mb-0 text-muted">© 2022 Team Delta</p>
-					<div className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
-						<img className="bi me-2" height="32" src={Logo} alt="world-bank-logo"></img>
-					</div>
-				</footer>
-			</Container>
-		</Container>
-	);
+  return (
+    <Container className="py-4">
+      <header className="border-bottom pb-3 mb-4">
+        <Navbar>
+          <Container>
+            <Navbar.Brand href="/">
+              <img src={Logo} height={50} alt="world-bank-logo" />{" "}
+              <Button variant="primary">Search</Button>{" "}
+              <Button variant="primary">History</Button>
+            </Navbar.Brand>
+          </Container>
+          <Button
+            variant="primary"
+            className="text-nowrap"
+            onClick={props.logOut}
+          >
+            Log Out
+          </Button>
+        </Navbar>
+      </header>
+      <div className="mb-4 bg-light rounded-3">
+        {data ? getGraph() : <p>Loading...</p>}
+      </div>
+      <Container>
+        <footer className="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
+          <p className="col-md-4 mb-0 text-muted">© 2022 Team Delta</p>
+          <div className="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+            <img
+              className="bi me-2"
+              height="32"
+              src={Logo}
+              alt="world-bank-logo"
+            ></img>
+          </div>
+        </footer>
+      </Container>
+    </Container>
+  );
 }
--- world-bank-app/src/components/PageSelector.js
+++ world-bank-app/src/components/PageSelector.js
@@ -6,62 +6,64 @@ import OneCountryOneIndicatorOneYear from "./OneCountryOneIndicatorOneYear";
 import TwoCountriesOneIndicator from "./TwoCountriesOneIndicator";
 import TwoCountriesOneIndicatorOneYear from "./TwoCountriesOneIndicatorOneYear";
 
-
 function PageSelector(props) {
-	function selectPage() {
-		if (!props.countries || !props.year || !props.indicator) {
-			return <Navigate replace to="/search" />;
-		} else if (
-			props.countries.length === 1 &&
-			props.indicator !== "All" &&
-			props.year === "All"
-		) {
-			return <OneCountryOneIndicator country={props.countries} indicator={props.indicator} />;
-		} else if (
-			props.countries.length === 1 &&
-			props.indicator !== "All" &&
-			props.year !== "All"
-		) {
-			return (
-				<OneCountryOneIndicatorOneYear
-					country={props.countries}
-					indicator={props.indicator}
-					year={props.year}
-				/>
-			);
-		} else if (
-			props.countries.length === 2 &&
-			props.indicator !== "All" &&
-			props.year === "All"
-		) {
-			return (
-				<TwoCountriesOneIndicator countries={props.countries} indicator={props.indicator} />
-			);
-		} else if (
-			props.countries.length === 2 &&
-			props.indicator !== "All" &&
-			props.year === "All"
-		) {
-			return (
-				<TwoCountriesOneIndicator countries={props.countries} indicator={props.indicator} />
-			);
-		} else if (
-			props.countries.length === 2 &&
-			props.indicator !== "All" &&
-			props.year !== "All"
-		) {
-			return (
-				<TwoCountriesOneIndicatorOneYear
-					countries={props.countries}
-					indicator={props.indicator}
-					year={props.year}
-				/>
-			);
+  function selectPage() {
+    if (!props.countries || !props.year || !props.indicator) {
+      return <Navigate replace to="/search" />;
+    } else if (
+      props.countries.length === 1 &&
+      props.indicator !== "All" &&
+      props.year === "All"
+    ) {
+      return (
+        <OneCountryOneIndicator
+          country={props.countries}
+          indicator={props.indicator}
+          logOut={props.logOut}
+        />
+      );
+    } else if (
+      props.countries.length === 1 &&
+      props.indicator !== "All" &&
+      props.year !== "All"
+    ) {
+      return (
+        <OneCountryOneIndicatorOneYear
+          country={props.countries}
+          indicator={props.indicator}
+          year={props.year}
+          logOut={props.logOut}
+        />
+      );
+    } else if (
+      props.countries.length === 2 &&
+      props.indicator !== "All" &&
+      props.year === "All"
+    ) {
+      return (
+        <TwoCountriesOneIndicator
+          countries={props.countries}
+          indicator={props.indicator}
+          logOut={props.logOut}
+        />
+      );
+    } else if (
+      props.countries.length === 2 &&
+      props.indicator !== "All" &&
+      props.year !== "All"
+    ) {
+      return (
+        <TwoCountriesOneIndicatorOneYear
+          countries={props.countries}
+          indicator={props.indicator}
+          year={props.year}
+          logOut={props.logOut}
+        />
+      );
+    }
+  }
 
-		}
-	}
-
-	return selectPage();
+  return selectPage();
 }
 
 export default PageSelector;
